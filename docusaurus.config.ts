036166529d3e8a8b40ba613490158ae1f488991a
transaurus <pageturner.translation@gmail.com>
2025-06-06 13:42:08 +0800

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -29,7 +29,7 @@ const config: Config = {
   url: 'https://moonbitlang.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/moonbit-textbook/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
